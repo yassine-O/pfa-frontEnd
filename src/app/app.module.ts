7c6c0e4a08738fd903d14c300317f3b79600ca50
nashmoha-7 <psinassh@gmail.com>
2020-05-07 12:42:19 +0000

    login -sign up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './sign-in-up/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sign-in-up/login/login.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { RegisterForm } from '../register-form';
+import { HttpClient} from '@angular/common/http';
+import {Token} from '../token';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  registerForm=new RegisterForm();
+  error:string="";
+  errorLogin:string=""
+  username:string="";
+  password:String="";
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(): void {
+  }
+  onselectRole(role:string){
+      this.registerForm.role=role;
+      console.log(role);
+  }
+  register(){
+    console.log(this.registerForm)
+    this.http.post("http://localhost:8080/save",this.registerForm).subscribe(
+      (data)=>{
+        console.log(data)
+        let token=new Token();
+        let jwt=token.extract(data,"token")//extract token from data
+        token.saveToken(jwt,"token");
+      },
+      (error)=>{
+          console.log(error);
+         this.error=error.error;
+      }
+    )
+  }
+  onLogin(){
+    console.log(this.username +this.password)
+    let criden={
+      "username":this.username,
+      "password":this.password
+    }
+    this.login(criden)
+  }
+  login(cridentia) {
+
+    this.http.post('http://localhost:8080/authenticate', cridentia).subscribe(data => {
+      console.log(data);
+
+      let token: Token = new Token();
+      let jwtToken = token.extract(data, "jwt");
+      token.saveToken(jwtToken, "token")
+
+
+    },
+      error => {
+        if (error.status == 401) {
+          this.errorLogin = error.error;
+
+        }
+
+      })
+  }
+}
